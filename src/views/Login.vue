cb23204e715d722a0c048c0d9025d1f5306fbcb1
heyongjian <heyongjian@taqu.cn>
2021-01-15 17:54:13 +0800

    登录注册页面

---
+++ src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <van-form>
+      <van-field
+        v-model="form.username"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="form.password"
+        type="password"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      form:{}
+    }
+  },
+  methods:{
+    submit(){}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
